8f2915ad85b6b6647f2e17d119f254390ff052c3
h186497 <186497@stud.hvl.no>
2024-10-05 17:48:50 +0200

    oppdatering oppgave1

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// TODO
+		GPSPoint test = new GPSPoint(1,2.0,3.0,5);
+		
+		System.out.println(test.getTime());
+		
+		test.setTime(2);
+		
+		System.out.println(test.getTime());
+
+		System.out.println(test.toString());
 		
 	}
 
