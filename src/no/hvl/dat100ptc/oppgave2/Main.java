8b75995be66fa2afee7bb74c8f259a59e19a0e65
h186497 <186497@stud.hvl.no>
2024-10-22 13:13:07 +0200

    ferdig del 2?

--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -6,8 +6,22 @@ public class Main {
 
 	
 	public static void main(String[] args) {
-
-		// TODO
+        // Oppretter to GPSPoint-objekter
 		
+        GPSPoint point1 = new GPSPoint(1, 60.385390, 5.332200, 50.0);
+        GPSPoint point2 = new GPSPoint(2, 60.384990, 5.331950, 49.0);
+
+        // Oppretter GPSData-objekt med plass til to GPSPoint-objekter
+        GPSData test = new GPSData(2);
+
+        // Setter inn de to GPSPoint-objektene i GPSData
+        test.insertGPS(point1);
+        test.insertGPS(point2);
+
+        // Skriver ut informasjon om GPSPoint-objektene
+        
+        test.print();
+        
+        
 	}
 }
